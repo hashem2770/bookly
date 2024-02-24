00df2f978db00aabea626176337d6222c5202bd9
hashem <hashemhassen0@gmail.com>
2024-02-24 16:34:21 +0200

    fetch newest books impl

--- lib/features/home_view/data/repos/home_repo_impl.dart
+++ lib/features/home_view/data/repos/home_repo_impl.dart
@@ -1,13 +1,29 @@
 import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/core/helper/api_service.dart';
 import 'package:bookly/features/home_view/data/models/BookModel.dart';
 import 'package:bookly/features/home_view/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class HomeRepoImpl extends HomeRepo{
+class HomeRepoImpl extends HomeRepo {
+  final ApiService _apiService;
+
+  HomeRepoImpl(ApiService apiService) : _apiService = apiService;
+
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var result = await _apiService.get(
+          endPoint:
+              'volumes?filtering=free-ebooks&sorting=newest&q=subject:health');
+
+      List<BookModel> books = [];
+      for (final book in result['items']) {
+        books.add(book);
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
@@ -15,5 +31,4 @@ class HomeRepoImpl extends HomeRepo{
     // TODO: implement fetchFeatureBooks
     throw UnimplementedError();
   }
-
 }
