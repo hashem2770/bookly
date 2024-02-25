56a96ff57ed0f865fb68574b61b3e0d25ee40e7a
hashem <hashemhassen0@gmail.com>
2024-02-25 12:49:21 +0200

    create feature books cubit

--- lib/features/home_view/presentation/manager/feature_books_cubit/feature_books_cubit.dart
+++ lib/features/home_view/presentation/manager/feature_books_cubit/feature_books_cubit.dart
@@ -1,10 +1,24 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly/features/home_view/data/repos/home_repo_impl.dart';
+import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../../../../core/errors/failure.dart';
 import '../../../data/models/BookModel.dart';
 
 part 'feature_books_state.dart';
 
 class FeatureBooksCubit extends Cubit<FeatureBooksState> {
-  FeatureBooksCubit() : super(FeatureBooksInitial());
+  FeatureBooksCubit(this.homeRepoImpl) : super(FeatureBooksInitial());
+  HomeRepoImpl homeRepoImpl;
+
+  Future<void> fetchFeatureBooks() async {
+    emit(FeatureBooksLoading());
+    Either<Failure, List<BookModel>> result =
+        await homeRepoImpl.fetchFeatureBooks();
+    result.fold(
+      (failure) => emit(FeatureBooksFailure(failure.errMessage)),
+      (books) => emit(FeatureBooksSuccess(books)),
+    );
+  }
 }
--- lib/features/home_view/presentation/manager/feature_books_cubit/feature_books_state.dart
+++ lib/features/home_view/presentation/manager/feature_books_cubit/feature_books_state.dart
@@ -14,17 +14,13 @@ class FeatureBooksLoading extends FeatureBooksState {
 }
 class FeatureBooksFailure extends FeatureBooksState {
   final String errMessage;
-
   const FeatureBooksFailure(this.errMessage);
-
   @override
   List<Object> get props => [errMessage];
 }
 class FeatureBooksSuccess extends FeatureBooksState {
   final List<BookModel> books;
-
   const FeatureBooksSuccess(this.books);
-
   @override
   List<Object> get props => [books];
 }
